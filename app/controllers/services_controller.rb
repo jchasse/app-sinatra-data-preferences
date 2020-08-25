6d98e1b01efc218651036e5bca4faed5e8aebac9
jchasse <jonathan.r.chasse@gmail.com>
2020-08-25 16:42:53 -0500

    added show functionality for user services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,6 +2,8 @@ class ServicesController < ApplicationController
 
     get '/services' do
         if logged_in?
+            @user = User.find_by(params)
+            binding.pry
             erb :'services/index'
         else
             redirect to '/users/signup'
