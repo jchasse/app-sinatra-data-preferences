8b6bd34cb84af5eaa6cdef7f2039638ee63c3f3c
jchasse <jonathan.r.chasse@gmail.com>
2020-08-26 10:52:22 -0500

    added edit functionality to service model

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -16,7 +16,6 @@ class ServicesController < ApplicationController
 
     post '/services' do
         service = Service.new(params)
-        binding.pry
         if service.save
             service.user_id = session[:user_id]
             redirect to "/services/#{service.id}"
@@ -26,9 +25,33 @@ class ServicesController < ApplicationController
         end
     end
 
-    get '/services/:id' do
-
+    get '/services/:id/edit' do
+        redirect_if_not_logged_in
+        @service = Service.find_by(params)
+        erb :'services/edit'
     end
+
+    get '/services/:id' do
+        redirect_if_not_logged_in
+        if @service = Service.find_by(params)
+            erb :'services/show'
+        else
+            redirect '/services'
+        end
+    end
+
+    patch '/services/:id' do
+        redirect_if_not_logged_in
+        binding.pry
+        @service = Service.find_by(params)
+        # if is_not_blank?(params)
+        #     @service.content = params[:content]
+        #     @tweet.save
+        #     redirect to "/tweets/#{@tweet.id}"
+        # else
+        #     redirect to "/tweets/#{@tweet.id}/edit"
+        # end
+      end
 
 
 end
